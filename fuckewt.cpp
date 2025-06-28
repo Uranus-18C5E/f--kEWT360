6f14b9454c11effaa06890ed66a6c7ae22bee563
Uranus_18C5E <mc2748246086@outlook.com>
2025-06-28 14:47:23 +0800

    Add files via upload
    有……有……bug！

--- fuckewt.cpp
+++ fuckewt.cpp
@@ -3,16 +3,22 @@
 int main()
 {
     HDC Screen=GetDC(NULL);
+    int Classx=1469;
+    int Classy=325;
+    double screenx=GetSystemMetrics(SM_CXSCREEN)/1920;
+    double screeny=GetSystemMetrics(SM_CYSCREEN)/1080;
     LANDEXIELE:
-    COLORREF Pause=GetPixel(Screen,692,641);//检测 认真度检测 按钮上的蓝色
-    COLORREF End=GetPixel(Screen,480,574);//EWT植物人的喇叭上的深蓝色
-    COLORREF End1=GetPixel(Screen,406,519);//EWT植物人的帽子上的棕色
+    COLORREF Pause=GetPixel(Screen,692*screenx,641*screeny);//检测 认真度检测 按钮上的蓝色
+    COLORREF End=GetPixel(Screen,480*screenx,574*screeny);//EWT植物人的喇叭上的深蓝色
+    COLORREF End1=GetPixel(Screen,406*screenx,519*screeny);//EWT植物人的帽子上的棕色
+    POINT pt;
+    GetCursorPos(&pt);
     while(End!=RGB(75,126,251)&&End1!=RGB(170,103,108))
     {
         if(Pause==RGB(46,134,255)||Pause==RGB(0,102,255))
         {   
             //移动鼠标
-            SetCursorPos(692,641);
+            SetCursorPos(692*screenx,641*screeny);
             mouse_event(MOUSEEVENTF_LEFTDOWN|MOUSEEVENTF_LEFTUP,0,0,0,0);//点击
         }
         Sleep(1024);
@@ -21,18 +27,20 @@ int main()
     End1=GetPixel(Screen,406,519);//EWT植物人的帽子上的棕色
 
     }
-    std::cout<<"本节课程结束\n若需要重新启用，请按1；不需要，请按0";
-    int ans;
-    scanf("%d",ans);
-    if(ans!=0)
+    if(End==RGB(75,126,251)&&End1==RGB(170,103,108)&&pt.y<1034*screeny)
     {
-        for(int i=10;i>0;i--)
-        {
-            std::cout<<"好的，将于"<<i<<"秒后重新检测按钮";
-        }
-        system("cls");
-        goto LANDEXIELE;
+     //两课程之间相差84px
+    SetCursorPos((Classx+84)*screenx,(Classy+84)*screeny);
+    mouse_event(MOUSEEVENTF_LEFTDOWN|MOUSEEVENTF_LEFTUP,0,0,0,0);
+    Classy+84*screeny;
+    goto LANDEXIELE;
+    }
+    else if(End==RGB(75,126,251)&&End1==RGB(170,103,108)&&pt.y>1034*screeny)
+    {
+        std::cout<<"此页课程已经播放完毕，请下滑鼠标或者更换下一天的课程";
     }
     ReleaseDC(NULL,Screen);
+    system("pause");
     return 0;
+
 }
